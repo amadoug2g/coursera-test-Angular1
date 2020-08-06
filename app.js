dc1bef03b0a29df906b989c03a7867366431df24
Amadou <amadou_g@hotmail.fr>
2020-08-06 19:53:57 +0200

    Updating files

--- app.js
+++ app.js
@@ -26,58 +26,68 @@
     angular.module('DIApp', [])
     .controller('DIController', DIController);
 
-    function DIController ($scope, $filter){
-        $scope.name = "Amadou. C";
-        $scope.text = "";
+    function DIController ($scope){
+        $scope.list = "Apples";
         $scope.output = "";
-        $scope.outputLen = 1;
-        $scope.msg = "Waiting";
+        $scope.listSize = 1;
+        $scope.msg = "";
 
-        $scope.upper = function() {
-            var upperCase = $filter('uppercase');
-            $scope.name = upperCase($scope.name);
+        $scope.processInput = function() {
+            getInput();
+            makeList();
+            displayMessage();
         }
 
-        $scope.lower = function() {
-            var lowerCase = $filter('lowercase');
-            $scope.name = lowerCase($scope.name);
-        }
-
-        $scope.getText = function() {
-            var temp = $scope.name;
+        /**
+         * Retrieves elements from field
+         */
+        function getInput() {
+            var temp = $scope.list;
             $scope.text = temp;
         }
 
-        $scope.listElm = function() {
-            var tmp = $scope.name;
-            tmp = tmp.split(',')
-            tmp = trimming2(tmp);
-            $scope.output = tmp;
-            $scope.outputLen =  tmp.length;            
+        /**
+         * Creates a list from the user's input
+         */
+        function makeList() {
+            var sortedList = $scope.list;
+            sortedList = sortedList.split(',')
+            sortedList = emptyCheck(sortedList);
+            $scope.output = sortedList;
+            $scope.listSize =  sortedList.length;            
         }
 
-        $scope.message = function() {
-            if ($scope.outputLen > 0 && $scope.outputLen < 4) {
+        /**
+         * Displays the message after checking user's input
+         */
+        function displayMessage() {
+            if ($scope.listSize > 0 && $scope.listSize < 4) {
                 $scope.msg = "Enjoy!";
             } else {
                 $scope.msg = "Too Much!";
             }
         }
 
-        function trimming (list) {
-            var trim = list.filter(Boolean);
-            return trim;
+        /**
+         * Gets rid of null and empty elements from the array
+         * @param {array} list Array of elements from user input
+         */
+        function nullCheck (list) {
+            var trimmedList = list.filter(Boolean);
+            return trimmedList;
         }
 
-        function trimming2 (list) {
-            //var trim = list.filter(function(el) { return el; });
-
-            var trim = list.filter(function(el) { 
+        /**
+         * Gets rid of elements containing nothing but space from the array
+         * @param {array} list Array of elements from user input
+         */
+        function emptyCheck (list) {
+            var trimmedList = list.filter(function(el) { 
                 return el.trim(); 
             });
 
-            trimming(trim);
-            return trim;
+            nullCheck(trimmedList);
+            return trimmedList;
         }
     }
 })();
